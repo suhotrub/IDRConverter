e54f9f06d8efc66be8ee7561f779793a4185a855
yolo <suhotrub1@gmail.com>
2023-02-03 01:10:26 +0800

    aye

--- flaskcode.py
+++ flaskcode.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from IDRConverter.ratescalculator import calculateRates
+from IDRConverter.scheduler import scheduleTask
 from prettytable import PrettyTable
 
 app = Flask(__name__, static_url_path='',static_folder='static',template_folder='template')
@@ -21,5 +22,25 @@ def main():
 
 	return variablesTable.get_html_string() + "<br><br>" + resultTable.get_html_string() + "<br>" + render_template("template.html")
 
+
+@app.before_first_request
+def activate_job():
+    scheduleTask()
+
+def start_runner():
+    def start_loop():
+        not_started = True
+        while not_started:
+            try:
+                r = requests.get('http://127.0.0.1:5000/')
+                if r.status_code == 200:
+                    not_started = False
+            except:
+                print('Server not yet started')
+            time.sleep(2)
+    thread = threading.Thread(target=start_loop)
+    thread.start()
+
 def startBackend():
+	start_runner()
 	app.run(use_reloader=False)
