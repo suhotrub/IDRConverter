8da924b6c7b93dce9d2ce216575a15ec9a6ba7d9
yolo <suhotrub1@gmail.com>
2023-02-03 01:07:17 +0800

    aye

--- __init__.py
+++ __init__.py
@@ -1,9 +1,7 @@
-from IDRConverter.scheduler import scheduleTask
 from IDRConverter.flaskcode import startBackend, app
 from IDRConverter.databasecode import createDatabase
 
 createDatabase()
-scheduleTask()
 
 if __name__ == "__main__":
 	startBackend()
