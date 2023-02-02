6ae955632295c3eacf3cc376408235dce2cf08ea
yolo <suhotrub1@gmail.com>
2023-02-03 00:39:18 +0800

    aye

--- scheduler.py
+++ scheduler.py
@@ -2,18 +2,18 @@ from IDRConverter.databasecode import writeToDatabase
 from IDRConverter.ratescalculator import calculateRates
 from apscheduler.schedulers.background import BackgroundScheduler
 import sys
+import atexit
 
 def gatherDataAndPutToStorage():
-	print("hello.")
+	print('scheduling', file=sys.stderr)
 	rates = calculateRates()
-	print("loaded.")
 	writeToDatabase(rates[1])
-	print("wrote.")
 
 def scheduleTask():
-	
 	print('This is error output', file=sys.stderr)
 	print('This is standard output', file=sys.stdout)
 	sched = BackgroundScheduler(daemon=True)
-	sched.add_job(gatherDataAndPutToStorage,'interval',minutes=60)
+	sched.add_job(func=gatherDataAndPutToStorage, trigger="interval", minutes=1)
 	sched.start()
+	atexit.register(lambda: sched.shutdown())
+
