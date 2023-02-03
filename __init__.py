4f2440accb11c41d19397ac4ee628ef391663682
a.gorbachev <a.gorbachev@tinkoff.ru>
2023-02-03 16:22:03 +0800

    aye

--- __init__.py
+++ __init__.py
@@ -4,23 +4,23 @@ import sys
 
 def start_runner():
 	print('Trying to start server', file=sys.stderr)
-    def start_loop():
-        not_started = True
-        while not_started:
-            print('In start loop', file=sys.stderr)
-            try:
-                r = requests.get('http://127.0.0.1:5000/')
-                if r.status_code == 200:
-                    print('Server started, quiting start_loop', file=sys.stderr)
-                    not_started = False
-                print(r.status_code)
-            except:
-                print('Server not yet started', file=sys.stderr)
-            time.sleep(2)
+	def start_loop():
+		not_started = True
+		while not_started:
+			print('In start loop', file=sys.stderr)
+			try:
+				r = requests.get('http://127.0.0.1:5000/')
+				if r.status_code == 200:
+					print('Server started, quiting start_loop', file=sys.stderr)
+					not_started = False
+				print(r.status_code)
+			except:
+				print('Server not yet started', file=sys.stderr)
+			time.sleep(2)
 
-    print('Started runner', file=sys.stderr)
-    thread = threading.Thread(target=start_loop)
-    thread.start()
+	print('Started runner', file=sys.stderr)
+	thread = threading.Thread(target=start_loop)
+	thread.start()
 
 createDatabase()
 start_runner()
