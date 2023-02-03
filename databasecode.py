156f738e71abb39742ca7385dd18ee6f6c0bbe59
a.gorbachev <a.gorbachev@tinkoff.ru>
2023-02-03 17:04:39 +0800

    aye

--- databasecode.py
+++ databasecode.py
@@ -1,19 +1,24 @@
 import csv
 import time
 import os
-path = "/var/www/FlaskApp/IDRConverter/static/database.csv"    
+path = "/var/www/FlaskApp/IDRConverter/static/"
+filepath = path+"database.csv"
 
 def createDatabase():
-    if os.path.isfile(path):
+    if os.path.isfile(filepath):
         return
 
-    with open(path, 'w+', newline='') as file:
+    try:
+        os.mkdir(path)
+    except OSError as error:
+        print(error)    
+    with open(filepath, 'w+', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["Time", "Stock price","Tinkoff CNY","Korona GEL USDT", "Korona GEL Credo USDT", "Tinkoff USDT", "Unistream GEL USDT", "TBC USD IDR"])
 
 
 def writeToDatabase(table):
-    with open(path, 'a', newline='') as file:
+    with open(filepath, 'a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow([
             round(time.time()*1000),
@@ -21,7 +26,7 @@ def writeToDatabase(table):
             table["Tinkoff CNY"],
             table["Korona GEL USDT"],
             table["Korona GEL Credo USDT"],
-            table["Tinkoff USDT"],
+                table["Tinkoff USDT"],
             table["Unistream GEL USDT"],
             table["TBC USD IDR"],
          ])
